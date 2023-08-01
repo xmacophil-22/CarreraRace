91396063860835079e500916d9d9b53c39f614d2
Maya Kuhlmann <maya.zoe@gmx.de>
2023-08-01 12:33:35 +0200

    changed location of files

--- app/src/main/java/com/example/carrerarace/Callback.java
+++
@@ -1,8 +0,0 @@
-package com.example.carrerarace;
-
-import org.json.JSONObject;
-
-////////////////////////////////////Callback to use Result from Request
-public interface Callback {
-    void myCallback(JSONObject result);
-}
